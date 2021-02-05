58e0de8ced25ed2295fbd6ad754fd2ac5459fe11
KaisarCode <kaisar@kaisarcode.com>
2021-02-05 16:00:01 -0300

    correct variable

--- tagfun.class.php
+++ tagfun.class.php
@@ -1,17 +1,17 @@
 <?php
 namespace KC;
 
-Class TagFun {
-    // Replace tags by function output
+// Replace tags by function output
+class Tagfun {
     static function r($str, $tg1, $tg2, $fun) {
         $t1 = preg_quote($tg1, '/');
         $t2 = preg_quote($tg2, '/');
         $rx = "/$t1([\\s\\S]*?)$t2/";
-        $prc = preg_replace_callback;
-        $str = $prc($rx, function($m)
-        use (&$fun) { return $fun($m[1]);
-        }, $str); preg_match($rx, $str) &&
-        $str = self::r($str, $tg1, $tg2, $fun);
+        $str = preg_replace_callback
+        ($rx, function($m) use (&$fun)
+        { return $fun($m[1]);}, $str);
+        preg_match($rx, $str) && $str =
+        self::r($str, $tg1, $tg2, $fun);
         return $str;
     }
 }
--- tagfun.php
+++ tagfun.php
@@ -4,10 +4,10 @@ function tagfun($str, $tg1, $tg2, $fun) {
     $t1 = preg_quote($tg1, '/');
     $t2 = preg_quote($tg2, '/');
     $rx = "/$t1([\\s\\S]*?)$t2/";
-    $prc = preg_replace_callback;
-    $str = $prc($rx, function($m)
-    use (&$fun) { return $fun($m[1]);
-    }, $str); preg_match($rx, $str) &&
-    $str = tagfun($str, $tg1, $tg2, $fun);
+    $str = preg_replace_callback
+    ($rx, function($m) use (&$fun)
+    { return $fun($m[1]);}, $str);
+    preg_match($rx, $str) && $str =
+    tagfun($str, $tg1, $tg2, $fun);
     return $str;
 }
