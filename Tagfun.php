3fbbaca0197d9b098f4ff8e740362109ad5809db
KaisarCode <kaisar@kaisarcode.com>
2021-05-24 15:41:02 -0300

    Replace by object property

--- Tagfun.php
+++ Tagfun.php
@@ -1,6 +1,8 @@
 <?php
 // Replace tags by function output
 class Tagfun {
+    
+    // Replace by callback
     static function r($str, $tg1, $tg2, $fun) {
         $t1 = preg_quote($tg1, '/');
         $t2 = preg_quote($tg2, '/');
@@ -11,5 +13,13 @@ class Tagfun {
         preg_match($rx, $str) && $str =
         self::r($str, $tg1, $tg2, $fun);
         return $str;
+    }
+    
+    // Replace by object property
+    static function o($str, $tg1, $tg2, $obj) {
+        return self::r($str, $tg1, $tg2,
+        function($c) use (&$obj){
+        $out = ''; isset($obj->$c) &&
+        $out = $obj->$c; return $out; });
     }
 }
